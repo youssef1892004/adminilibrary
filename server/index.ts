534b075d4b34d97808d51b68a055bb4b48eaec30
youssef1892004 <essayoussef82@gmail.com>
2025-08-21 05:29:10 +0300

    Upload new ilibrary-dashboard project

--- server/index.ts
+++ server/index.ts
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = 3000;
 server.listen({
   port,
   host: "127.0.0.1",
